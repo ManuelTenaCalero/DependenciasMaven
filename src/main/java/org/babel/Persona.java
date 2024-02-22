b92ef5c764d36e08256990c14d1164bd1d4e8d12
Tena Calero <mtena@sice-outsourcing.com>
2024-02-22 12:37:30 +0100

    Ejercicio Persona

--- src/main/java/org/babel/Persona.java
+++ src/main/java/org/babel/Persona.java
@@ -1,4 +1,20 @@
 package org.babel;
 
 public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
 }
+
